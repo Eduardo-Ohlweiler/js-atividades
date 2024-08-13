99adac99d18ef818858382007d37f2c5bf8b662b
Eduardo Rodrigo Ohlweiler <136762851+Eduardo-Ohlweiler@users.noreply.github.com>
2024-08-13 20:11:17 -0300

    Add files via upload

---
+++ parouimpar/index.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form");
+const resposta = document.getElementById("resposta");
+
+form.addEventListener("submit", function(e){
+    const numero = parseInt(form.innumero.value);
+    if (numero % 2 === 0) {
+        resposta.innerText = `O número ${numero} é par`;
+    } else {
+        resposta.innerText = `O número ${numero} é ímpar`;
+    }
+
+    e.preventDefault();
+
+})
